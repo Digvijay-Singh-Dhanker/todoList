930ece33c0a3915c47b29ff20061ba1e3f325d92
Digvijay Singh Dhanker <digvijay.dhanker@gmail.com>
2021-05-06 15:01:28 +0530

    Added the project

---
+++ controllers/users_controller.js
@@ -0,0 +1,32 @@
+const TODO=require('../models/Todo');
+
+module.exports.create=function(req,res){        //Data added to the database here
+    TODO.create({
+        description:req.body.description,
+        date:req.body.date,
+        category:req.body.category
+    },function(err,newTodo){
+        if(err){
+            console.log('Error in creating in TODO');
+            return;
+        }
+        console.log(newTodo);
+        return res.redirect('/');
+    });
+};
+
+module.exports.removeTask=function(req,res){        //Delete function for deleting data from database
+    var id=req.query;
+    console.log(id);
+    var count=Object.keys(id).length;
+    console.log(count);
+    for(let i=0;i<count;i++){
+        TODO.findByIdAndDelete(Object.keys(id),function(err){
+            if(err){
+                console.log("Error");
+                return;
+            }
+        });
+    }
+    return res.redirect('back');
+}
